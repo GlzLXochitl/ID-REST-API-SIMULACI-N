d3996933c43f54536dc2fc2670edaeb229d8427b
GlzLXochitl <glzlxochitl@gmail.com>
2024-02-11 20:52:24 -0600

    fix: extra files were deleted

---
+++ array_metods_pt2_hw2.1/array_methdos_api.js
@@ -0,0 +1,36 @@
+/*Debes utilizar find, filter, map, some, every, reduce)
+    - Las respuestas deben tener la siguiente estructura
+    o Debe ser una estructura Object/JSON/Dictionary/Map (key:value) con los siguientes
+    atributos:
+    ▪ code (el código de estado de la respuesta)
+    ▪ body (el cuerpo de la respuesta)
+    ▪ msg (un mensaje informativo)
+o Se deben utilizar los códigos vistos en las clases.
+o Es obligatorio el uso de retornos anticipados/cláusulas de guarda.
+o Debe crear una función para formatear sus respuestas y validar todos los posibles
+posibles.*/
+
+
+
+/*• Cree las siguientes 8 funciones (como función y como función de flecha) para simular una API REST.
+o getBook, toma el título de un libro O ISBN y lo devuelve si existe.
+o getBooks, devuelve todos los libros existentes.
+o addBook, agrega un nuevo libro a la matriz de libros y devuelve el libro creado, y el
+nueva matriz, incluido el nuevo libro.
+o removeBookByTitleOrISBN, toma un título O ISBN y, si lo encuentra, elimina el
+elemento de la matriz, devuelve el elemento eliminado y la nueva matriz.
+o filterBy, el primer parámetro será la propiedad de filtrado (género, autor o editor), el
+el segundo será la cadena que se está buscando. Debes devolver todos los libros que coincidan.
+la condición.
+o listBooks, devuelve una lista de todos los libros en el siguiente formato: Título - Autor - Año.
+o getBooksByYear, devuelve todos los libros de un año determinado.
+o géneroDisponibilidad total, devuelve verdadero o falso si todos los libros de un género determinado tienen stock
+disponible.
+o géneroPartialAvailability, devuelve verdadero o falso si al menos UN libro de un género determinado
+tiene disponibilidad de stock.
+o getCountBy, el primer parámetro será la propiedad de recuento (género, autor o editor),
+debes devolver un nuevo objeto con el nombre de la propiedad que estás contando y
+el contador.*/
+
+
+
